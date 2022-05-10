4740e8c73e1d6cd18655cd626eaa4e0b42edc96c
tomoropy <ucchi1910@gmail.com>
2022-05-10 10:52:40 +0900

    step5

--- VendingMachine/Suica_class.rb
+++ VendingMachine/Suica_class.rb
@@ -1,15 +1,22 @@
 class Suica
 	attr_accessor :balance
+	attr_reader   :sex, :age
 
-	def initialize(balance)
+	def initialize(balance, sex, age)
 		if balance >= 100
 			@balance = balance
+			@sex = sex
+			@age = age
 		else 
 			puts 'error : チャージ金額は100円以上にしてください'
 		end
 	end
 
 	def show_balance
-		puts "現在のSuicaの残高は #{@balance} 円です。"
+		puts "このSuicaの残高は #{@balance} 円です。"
+	end
+
+	def show_info
+		puts "このSuicaの持ち主は #{@sex} で、年齢は #{@age}歳 です。"
 	end
 end
--- VendingMachine/VendingMachine_class.rb
+++ VendingMachine/VendingMachine_class.rb
@@ -1,85 +1,117 @@
 require "./Suica_class.rb"
-
 module VendingMachine
 
-	@@stock = [
-		{ name: "コーラ", price: 120, count: 5 }
+	@@stocks = [
+		{name: "Cola", price: 120, count: 5}
 	]
-
-	# 売上(初期値は0)
 	@@sales = 0
 
+	@@sales_histories = []
+
 	class << self
-		# 自販機に飲料を追加
-		def add_drink( name, price, count )
-			@@stock << { name: name, price: price, count: count }	
-		end
 
 		# 自販機の在庫内容を表示
-		def show_stock
-			@@stock.each do |item|
-				puts "商品名 : #{item[:name]}  価格 : #{item[:price]}円  在庫 : #{item[:count]}個"
+		def show_stocks
+			puts "在庫内容".center(40, "*")
+			@@stocks.each do |item|
+				puts  "商品名 : " + item[:name].to_s.ljust(10) +
+							" 値段 : "  + item[:price].to_s.ljust(4) +
+							" 在庫数 : " + item[:count].to_s.ljust(2)
 			end
+			puts "*" * 44
 		end
 
-		#売上を表示
-		def show_sales
+		# 売上を表示
+		def show_sales_money
 			puts "現在の自販機の売上は #{@@sales} 円です。"
 		end
 
+		# 販売履歴を表示
+		def show_sales_history
+			puts "販売履歴".center(36, "*")
+			@@sales_histories.each_with_index do |history, i|
+				puts "#{i + 1}: 日時 #{history[:time]}, 性別 #{history[:sex]}, 年齢 #{history[:age]}歳"
+			end
+			puts "*" * 40
+		end
+		
+
+		# 自販機に飲料を追加
+		def add_drink(name, price, count)
+			@@stocks << { name: name, price: price, count: count }
+			puts name.to_s.ljust(10) + "が商品に追加されました！"
+		end
+
 		# 商品が購入可能か？（在庫量的に）
-		def is_there_stock?(name, count)
-			@@stock.each do |item|
+		def there_stock?(name, count)
+			@@stocks.each do |item|
 				if item[:name] == name
-					break item[:count] > count
+					break item[:count] >= count
 				end
 			end
 		end
 
 		# 購入合計金額を計算
 		def calc_to_total(name, count)
-			@@stock.each do |item|
+			@@stocks.each do |item|
 				if item[:name] == name
 					break item[:price] * count
 				end
 			end
 		end
+
+		# お金の処理
+		def money_proces(suica, total)
+			@@sales += total
+			suica.balance -= total
+		end
+		
+		# 販売履歴を登録する
+		def register_info(suica)
+			@@sales_histories << { time: Time.now.to_s.slice(0, 9), sex: suica.sex, age: suica.age }
+		end
 		
 		# 購入成立した場合の処理
 		def purchase_drink(suica, name, count)
-
 			total = calc_to_total(name, count)
-
-			if is_there_stock?(name, count) && suica.balance >= total
-
-				puts " #{name} を #{count} 本購入しました。"
-				# 売上に購入金額を追加
-				@@sales += total
-
-				# Suicaの残高を購入金額分へらす
-				suica.balance -= total
 			
+			if there_stock?(name, count) && suica.balance >= total
+				money_proces(suica, total)
+				register_info(suica)
+				puts name.to_s.ljust(10) + " を#{count}本購入しました。"
+
 			# 在庫数が足りない場合
-			elsif !is_there_stock?(name, count)
-				puts "error : 購入しようとしている商品は在庫がありません。"
+			elsif !there_stock?(name, count)
+				puts 'error : 購入しようとしている商品は在庫がありません。'
 
 			# Suicaの残高が足りない場合
-			elsif suica.balance < total 
-				puts "error : Suicaの残高が足りません。"
+			elsif suica.balance < total
+				puts 'error : Suicaの残高が足りません。'
 			end
 		end
 	end
 
 end
 
-VendingMachine.show_sales
-VendingMachine.is_there_stock?("コーラ",3)
-VendingMachine.add_drink("カルピス", 130, 10)
+# 初期商品在庫を表示
+VendingMachine.show_stocks
 
-suica1 = Suica.new(300)
+# 商品を追加
+VendingMachine.add_drink("Red bull", 200, 5)
+VendingMachine.add_drink("Water", 100, 5)
+
+# 在庫内容を表示
+VendingMachine.show_stocks
+
+# Suicaを発行
+suica1 = Suica.new(400, "男性", 22)
 suica1.show_balance
+suica1.show_info
 
-VendingMachine.purchase_drink(suica1, "カルピス", 2)
+# Red Bullと水を購入
+VendingMachine.purchase_drink(suica1, "Red bull", 1)
+VendingMachine.purchase_drink(suica1, "Water", 1)
 
-VendingMachine.show_sales
-suica1.show_balance
+# 自販機の売上と販売履歴を表示
+VendingMachine.show_sales_money
+VendingMachine.show_sales_history
